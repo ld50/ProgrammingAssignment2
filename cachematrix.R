cbd0052635ee72a06a309ebc58af37f65147429f
ld50 <thmpub+ld50@gmail.com>
2014-07-18 20:27:59 -0400

    finished makeCacheMatrix
    The first of two functions needed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## File creates two functions, the first makeCacheMatrix makes
+# a special cachable matrix like list. The inverse of this matrix
+# like list can be cached. The second function, cacheSolve, uses
+# the matrix like list structure and either returns a cached inverse
+# or inverts the matrix, caches the result, then returns the result.
+# The goal is to only do expensive calculations once.
 
-## Write a short comment describing this function
+
+## make a matrix which is actually a list of functions that
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse (cache)
+# get the value of the inverse (read the cache)
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
